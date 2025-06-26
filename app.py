2d5bd49ac495dd6755ba4b453f67e683906e1920
Frameit12 <frameitbot@gmail.com>
2025-06-26 07:06:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -709,14 +709,14 @@ def index():
         last_results = jobs
         global last_search_name
         last_search_name = title if title else "Job_Search"
-        return render_template("index.html", jobs=jobs, title=title, location=location, max_jobs=max_jobs, saved_searches=load_saved_searches())
+        return render_template("index.html", jobs=jobs, title=title, location=location, max_jobs=max_jobs, seniority=seniority, saved_searches=load_saved_searches())
     
     saved_searches = check_excel_files_for_searches(load_saved_searches())
     print("✅ Saved searches and their Excel status:")
     for s in saved_searches:
         print(f"- {s['name']}: has_excel = {s.get('has_excel')}")
 
-    return render_template("index.html", title="", location="", max_jobs=10, saved_searches=saved_searches)
+    return render_template("index.html", title="", location="", seniority="", max_jobs=10, saved_searches=saved_searches)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
